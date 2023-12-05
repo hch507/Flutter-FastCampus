51306e69833b9023396c30a8ed364cb79f48e0eb
chungHee Han <chunghuihan9@gmail.com>
2023-12-05 10:59:10 +0900

    update

--- compose_profilecard/app/src/main/java/com/example/compose_profilecard/CardData.kt
+++
@@ -1,8 +0,0 @@
-package com.example.compose_profilecard
-
-data class CardData(
-    val imageUri : String,
-    val imageDescription : String,
-    val author :String,
-    val description : String
-)
